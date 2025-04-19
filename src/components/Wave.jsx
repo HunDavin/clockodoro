78144622cbedf4f8e3ea05ef135c88f62ceeea18
Pychey <prakpycheyy@gmail.com>
2025-04-19 15:12:40 +0700

    Change Title And Logo Of Website To Clockodoro

--- src/components/Wave.jsx
+++ src/components/Wave.jsx
@@ -19,13 +19,13 @@ export default function Wave({ activeMode }) {
     } else if (activeMode.mode === "Short Break") {
       setWaveStyle({
         background: "#7684FF",
-        height: "79vh",
+        height: "80vh",
         minHeight: "555px"
       });
     } else if (activeMode.mode === "Long Break") {
       setWaveStyle({
         background: "#696A74",
-        height: "79vh",
+        height: "80vh",
         minHeight: "555px"
       });
     }
